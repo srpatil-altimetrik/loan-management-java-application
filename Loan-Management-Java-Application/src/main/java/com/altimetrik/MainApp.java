51d182b531dca0f6d96485e4c540915654d1ed18
srpatil <srpatil@AIPL6457LTIN603.altimetrik.com>
2025-05-17 20:45:51 +0530

    Final test for the check-up

--- Loan-Management-Java-Application/src/main/java/com/altimetrik/MainApp.java
+++ Loan-Management-Java-Application/src/main/java/com/altimetrik/MainApp.java
@@ -3,7 +3,7 @@ package com.altimetrik;
 public class MainApp {
 
 	public static void main(String[] args) {
-		System.out.println("Git is working fine...........");
+		System.out.println("Final test that git is working fine...................");
 	}
 
 }
